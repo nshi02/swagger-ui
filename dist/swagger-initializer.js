5a351d12e2eca50d26d8e3fa98590302068bbe74
Nadezhda Sharasheva <145916399+nshi02@users.noreply.github.com>
2024-09-16 16:26:58 +0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,11 +1,10 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/nshi02/PaymentBlock_swagger/blob/main/openapi.json",
+    url: "https://raw.githubusercontent.com/nshi02/PaymentBlock_swagger/main/openapi.json",
     dom_id: '#swagger-ui',
-    deepLinking: true,
+    deepLinking: true,  
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
